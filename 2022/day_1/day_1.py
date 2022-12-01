e87f0d10506bfe2971d4a01d2bfc7ae2222a73d0
fernando.machuca <fernando.machuca@guidesmiths.com>
2022-12-01 14:03:53 +0100

    chore(main): day 1 accomplished

--- 2022/day_1/day_1.py
+++ 2022/day_1/day_1.py
@@ -1,10 +1,13 @@
-def count_calories():
-
+def my_input():
     # Read from input file
     input = open('day_1_input.txt',"r")
     raw = input.read()        
     data = raw.split("\n")
+    return data
 
+def calories():
+
+    data = my_input()
     # Calculating each elf calories carried
     counter = 0
     totals = []
@@ -13,11 +16,16 @@ def count_calories():
             totals.append(counter)            
             counter = 0
         else:
-            counter = counter + int(d)
-    print(max(totals))
+            counter = counter + int(d)    
+    totals.sort(reverse=True)
+    return totals
 
 # Main function
 def main():
-    count_calories()
+    
+    # Part 1
+    print(f"The elf carrying the max amount of calories has a total of: {max(calories())}")
+    # Part 2    
+    print(f"The top three of elfs carrying calories have a total of:  {sum(calories()[0:3])}")
 
 main()
